5435674aff29bbc9293323fab24611a17dbb8097
JosefGst <tantam1992>
2024-06-06 10:42:13 +0800

    Add new py

--- src/obstacle_stopper_v3.py
+++ src/obstacle_stopper_v3.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+import numpy as np
 import rospy
 from geometry_msgs.msg import Twist
 from obstacle_detector.msg import Obstacles
+from nav_msgs.msg import Odometry
+from sound_play.msg import SoundRequest
 from sound_play.libsoundplay import SoundClient
 
 class ObstacleStopper:
@@ -16,10 +19,10 @@ class ObstacleStopper:
         self.stop_interval = rospy.Duration(3)  # Duration to wait after reaching max consecutive stops
 
         # Subscribers and Publishers
+        # rospy.Subscriber('/cmd_vel', Twist, self.cmd_vel_callback)
         rospy.Subscriber('/nav_vel', Twist, self.nav_vel_callback)
         rospy.Subscriber('/raw_obstacles', Obstacles, self.raw_obstacles_callback)
         self.tel_vel_pub = rospy.Publisher('/tel_vel', Twist, queue_size=10)
-        self.obstacles_pub = rospy.Publisher('/cleared_obstacles', Obstacles, queue_size=10)
 
         # Sound client for playing audio
         self.soundhandle = SoundClient()
@@ -29,60 +32,21 @@ class ObstacleStopper:
         self.consecutive_stop_count = 0
         self.stop_timer = rospy.Time(0)
 
-        self.linear_velocity = 0.0
-        self.obstacles_detected = False
-
+        self.linear_velocity = 0.0       
+    
     def nav_vel_callback(self, nav_vel_msg):
         # Extract linear velocity from cmd_vel message
         self.linear_velocity = nav_vel_msg.linear.x
 
     def raw_obstacles_callback(self, obstacles_msg):
-        if self.linear_velocity > 0.01:
-            # Check if there are any circles detected within the specified bounding box
-            self.obstacles_detected = False
-            for circle in obstacles_msg.circles:                
-                if self.is_within_front_bounding_box(circle.center.x, circle.center.y):
-                    rospy.loginfo("Received obstacle at x: %.2f, y: %.2f", circle.center.x, circle.center.y)
-                    rospy.loginfo("Obstacle detected in front of the robot within the front bounding box")
-                    self.obstacles_detected = True
-                    if not self.can_stop():
-                        return
-                    self.stop_robot()
-                    obstacles_msg = None
-                    self.clear_obstacles()
+        if self.linear_velocity > 0.0:
+            # Check if there are any circles detected
+            if obstacles_msg.circles:
+                rospy.loginfo("Obstacle detected in front of the robot")
+                if not self.can_stop():
                     return
-            if not self.obstacles_detected:
-                rospy.loginfo("No obstacles detected within the bounding box")
-        elif self.linear_velocity < -0.01:
-            # Check if there are any circles detected within the specified bounding box
-            self.obstacles_detected = False
-            for circle in obstacles_msg.circles:                
-                if self.is_within_back_bounding_box(circle.center.x, circle.center.y):
-                    rospy.loginfo("Received obstacle at x: %.2f, y: %.2f", circle.center.x, circle.center.y)
-                    rospy.loginfo("Obstacle detected behind of the robot within the back bounding box")
-                    self.obstacles_detected = True
-                    if not self.can_stop():
-                        return
-                    self.stop_robot()
-                    obstacles_msg = None
-                    self.clear_obstacles()
-                    return
-            if not self.obstacles_detected:
-                rospy.loginfo("No obstacles detected within the bounding box")
-        else:
-            return
-        
-    def is_within_front_bounding_box(self, x, y):
-        # Define the bounding box dimensions (2m length, 0.8m width)
-        if 0.2 <= x <= 2.2 and -0.4 <= y <= 0.4:
-            return True
-        return False
-
-    def is_within_back_bounding_box(self, x, y):
-        # Define the bounding box dimensions (2m length, 0.8m width)
-        if -2.0 <= x <= -1.0 and -0.4 <= y <= 0.4:
-            return True
-        return False
+                self.stop_robot()
+                return
 
     def stop_robot(self):
         # Play sound
@@ -103,12 +67,6 @@ class ObstacleStopper:
             self.tel_vel_pub.publish(tel_vel_msg)
         # After stop_duration seconds, resume navigation
         self.stopped = False
-
-    def clear_obstacles(self):
-        # Publish an empty Obstacles message to clear the circles
-        empty_obstacles_msg = Obstacles()
-        self.obstacles_pub.publish(empty_obstacles_msg)
-        rospy.loginfo("Published empty obstacles message to clear obstacles")
 
     def can_stop(self):
         # Check if the robot can stop based on the consecutive stop count and stop timer
