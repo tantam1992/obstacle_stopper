e4afc2cf38645561e65f4c07774ad62e7dfda426
tantam1992 <tm1992xb@gmail.com>
2024-06-25 15:42:47 +0800

    Modify cpp

--- src/obstacle_stopper_mux copy.cpp
+++ src/obstacle_stopper_mux copy.cpp
@@ -1,29 +1,30 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/Bool.h>
-#include <deque>
-#include <memory>
 
 class ObstacleStopperMux {
 public:
-    ObstacleStopperMux() : rate(7.5), obstacle_queue_size(3),
-                           front_queue(obstacle_queue_size, false),
-                           back_queue(obstacle_queue_size, false),
-                           rotate_queue(obstacle_queue_size, false) {
+    ObstacleStopperMux() {
+        ros::NodeHandle nh;
+
         cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
         sound_play_pub = nh.advertise<std_msgs::Bool>("/play_warning_sound", 10);
 
-        nav_vel_sub = nh.subscribe("/nav_vel", 10, &ObstacleStopperMux::nav_vel_callback, this);
-        tel_vel_sub = nh.subscribe("/tel_vel", 10, &ObstacleStopperMux::tel_vel_callback, this);
-        dock_vel_sub = nh.subscribe("/dock_vel", 10, &ObstacleStopperMux::dock_vel_callback, this);
-        is_in_front_sub = nh.subscribe("/is_in_front", 10, &ObstacleStopperMux::is_in_front_callback, this);
-        is_in_back_sub = nh.subscribe("/is_in_back", 10, &ObstacleStopperMux::is_in_back_callback, this);
-        is_in_rotate_sub = nh.subscribe("/is_in_rotate", 10, &ObstacleStopperMux::is_in_rotate_callback, this);
+        nav_vel_sub = nh.subscribe("/nav_vel", 10, &ObstacleStopperMux::navVelCallback, this);
+        tel_vel_sub = nh.subscribe("/tel_vel", 10, &ObstacleStopperMux::telVelCallback, this);
+        dock_vel_sub = nh.subscribe("/dock_vel", 10, &ObstacleStopperMux::dockVelCallback, this);
+        is_in_front_sub = nh.subscribe("/is_in_front", 10, &ObstacleStopperMux::isInFrontCallback, this);
+        is_in_back_sub = nh.subscribe("/is_in_back", 10, &ObstacleStopperMux::isInBackCallback, this);
+        is_in_rotate_sub = nh.subscribe("/is_in_rotate", 10, &ObstacleStopperMux::isInRotateCallback, this);
 
-        ROS_INFO("obstacle_stopper_mux started!");
+        is_in_front = false;
+        is_in_back = false;
+        is_in_rotate = false;
     }
 
     void run() {
+        ros::Rate rate(7.5);
+
         while (ros::ok()) {
             geometry_msgs::Twist stop_vel_msg;
             stop_vel_msg.linear.x = 0.0;
@@ -34,6 +35,11 @@ public:
             stop_vel_msg.angular.z = 0.0;
 
             geometry_msgs::Twist cmd_vel_msg = stop_vel_msg;
+
+            is_moving_forward = false;
+            is_moving_backward = false;
+            is_rotating = false;
+
             std_msgs::Bool play_sound_msg;
             play_sound_msg.data = false;
 
@@ -44,30 +50,31 @@ public:
                 ROS_INFO("dock_vel received");
                 cmd_vel_msg = *dock_vel;
             } else if (nav_vel) {
+                ROS_INFO("nav_vel receiving");
                 geometry_msgs::Twist nav_vel_msg = *nav_vel;
                 is_moving_forward = nav_vel_msg.linear.x > 0.1;
                 is_moving_backward = nav_vel_msg.linear.x < -0.1;
-                is_rotating = (-0.1 <= nav_vel_msg.linear.x && nav_vel_msg.linear.x <= 0.1) &&
-                              (nav_vel_msg.angular.z > 0.01 || nav_vel_msg.angular.z < -0.01);
+                is_rotating = (-0.1 <= nav_vel_msg.linear.x <= 0.1) && (nav_vel_msg.angular.z > 0.01 || nav_vel_msg.angular.z < -0.01);
+                ROS_INFO("nav_vel received");
 
                 is_able_to_move = false;
 
                 if (is_moving_forward) {
-                    if (!check_obstacle(front_queue)) {
+                    if (!is_in_front) {
                         ROS_INFO("forward enabled");
                         is_able_to_move = true;
                     }
                 }
 
                 if (is_moving_backward) {
-                    if (!check_obstacle(back_queue)) {
+                    if (!is_in_back) {
                         ROS_INFO("backward enabled");
                         is_able_to_move = true;
                     }
                 }
 
                 if (is_rotating) {
-                    if (!check_obstacle(rotate_queue)) {
+                    if (!is_in_rotate) {
                         ROS_INFO("rotate enabled");
                         is_able_to_move = true;
                     }
@@ -89,13 +96,14 @@ public:
             cmd_vel_pub.publish(cmd_vel_msg);
             tel_vel.reset();
             nav_vel.reset();
+            dock_vel.reset();
+
             ros::spinOnce();
             rate.sleep();
         }
     }
 
 private:
-    ros::NodeHandle nh;
     ros::Publisher cmd_vel_pub;
     ros::Publisher sound_play_pub;
     ros::Subscriber nav_vel_sub;
@@ -104,62 +112,42 @@ private:
     ros::Subscriber is_in_front_sub;
     ros::Subscriber is_in_back_sub;
     ros::Subscriber is_in_rotate_sub;
-    ros::Rate rate;
 
-    std::shared_ptr<geometry_msgs::Twist> nav_vel;
-    std::shared_ptr<geometry_msgs::Twist> tel_vel;
-    std::shared_ptr<geometry_msgs::Twist> dock_vel;
+    boost::shared_ptr<geometry_msgs::Twist const> nav_vel;
+    boost::shared_ptr<geometry_msgs::Twist const> tel_vel;
+    boost::shared_ptr<geometry_msgs::Twist const> dock_vel;
+
     bool is_in_front;
     bool is_in_back;
     bool is_in_rotate;
+
     bool is_moving_forward;
     bool is_moving_backward;
     bool is_rotating;
     bool is_able_to_move;
 
-    int obstacle_queue_size;
-    std::deque<bool> front_queue;
-    std::deque<bool> back_queue;
-    std::deque<bool> rotate_queue;
-
-    void nav_vel_callback(const geometry_msgs::Twist::ConstPtr& msg) {
-        nav_vel = std::make_shared<geometry_msgs::Twist>(*msg);
+    void navVelCallback(const geometry_msgs::Twist::ConstPtr& msg) {
+        nav_vel = msg;
     }
 
-    void tel_vel_callback(const geometry_msgs::Twist::ConstPtr& msg) {
-        tel_vel = std::make_shared<geometry_msgs::Twist>(*msg);
+    void telVelCallback(const geometry_msgs::Twist::ConstPtr& msg) {
+        tel_vel = msg;
     }
 
-    void dock_vel_callback(const geometry_msgs::Twist::ConstPtr& msg) {
-        dock_vel = std::make_shared<geometry_msgs::Twist>(*msg);
+    void dockVelCallback(const geometry_msgs::Twist::ConstPtr& msg) {
+        dock_vel = msg;
     }
 
-    void is_in_front_callback(const std_msgs::Bool::ConstPtr& msg) {
+    void isInFrontCallback(const std_msgs::Bool::ConstPtr& msg) {
         is_in_front = msg->data;
-        front_queue.push_back(msg->data);
-        if (front_queue.size() > obstacle_queue_size) {
-            front_queue.pop_front();
-        }
     }
 
-    void is_in_back_callback(const std_msgs::Bool::ConstPtr& msg) {
+    void isInBackCallback(const std_msgs::Bool::ConstPtr& msg) {
         is_in_back = msg->data;
-        back_queue.push_back(msg->data);
-        if (back_queue.size() > obstacle_queue_size) {
-            back_queue.pop_front();
-        }
     }
 
-    void is_in_rotate_callback(const std_msgs::Bool::ConstPtr& msg) {
+    void isInRotateCallback(const std_msgs::Bool::ConstPtr& msg) {
         is_in_rotate = msg->data;
-        rotate_queue.push_back(msg->data);
-        if (rotate_queue.size() > obstacle_queue_size) {
-            rotate_queue.pop_front();
-        }
-    }
-
-    bool check_obstacle(const std::deque<bool>& queue) {
-        return std::all_of(queue.begin(), queue.end(), [](bool v) { return v; });
     }
 };
 
